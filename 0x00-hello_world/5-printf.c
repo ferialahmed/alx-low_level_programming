3e7c4e06dbfa7f49e77639589efbec0b82f495d7
ferialahmed <ferialahmed38@gmail.com>
2023-02-16 17:10:32 -0800

    printing using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * main - Entry point
  *
@@ -8,8 +7,6 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
-
